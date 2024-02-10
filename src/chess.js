116214af3ef2a72d76d1e42a2aadab5c8f684dd5
Joey Bednar <joeyb654@gmail.com>
2024-02-09 19:33:37 -0800

    basic error handling, background steady on zoom, init formatter

--- src/chess.js
+++ src/chess.js
@@ -1,88 +1,110 @@
 async function chesscom(username) {
-  let obj;
-  try {
-    const response = await fetch(
-      "https://api.chess.com/pub/player/" + username + "/stats",
-    );
-    obj = await response.json();
-  } catch {
-    console.log("Error fetching chess.com rating");
-  }
+    let obj;
+    try {
+        const response = await fetch(
+            'https://api.chess.com/pub/player/' + username + '/stats'
+        );
 
-  let ratings = ["", "", "", ""];
-  const time_control = [
-    "chess_bullet",
-    "chess_blitz",
-    "chess_rapid",
-    "chess_daily",
-  ];
+        if (!response.ok) {
+            console.log('User not found.');
+            return;
+        }
 
-  for (let i = 0; i < time_control.length; i++) {
-    if (obj.hasOwnProperty(time_control[i])) {
-      ratings[i] = obj[time_control[i]].last.rating;
+        obj = await response.json();
+    } catch {
+        console.log('Error fetching chess.com rating');
+        return;
     }
-  }
 
-  return ratings;
+    let ratings = ['', '', '', ''];
+    const time_control = [
+        'chess_bullet',
+        'chess_blitz',
+        'chess_rapid',
+        'chess_daily'
+    ];
+
+    for (let i = 0; i < time_control.length; i++) {
+        if (obj.hasOwnProperty(time_control[i])) {
+            ratings[i] = obj[time_control[i]].last.rating;
+        }
+    }
+
+    return ratings;
 }
 async function lichess(username) {
-  try {
-    const response = await fetch("https://lichess.org/api/user/" + username);
-    const obj = await response.json();
-    return [
-      obj.perfs.bullet.rating,
-      obj.perfs.blitz.rating,
-      obj.perfs.rapid.rating,
-      //obj.perfs.classical.rating,
-      obj.perfs.correspondence.rating,
-    ];
-  } catch {
-    console.log("Error fetching lichess rating");
-  }
+    try {
+        const response = await fetch(
+            'https://lichess.org/api/user/' + username
+        );
+
+        if (!response.ok) {
+            console.log('User not found.');
+            return;
+        }
+
+        const obj = await response.json();
+        return [
+            obj.perfs.bullet.rating,
+            obj.perfs.blitz.rating,
+            obj.perfs.rapid.rating,
+            //obj.perfs.classical.rating,
+            obj.perfs.correspondence.rating
+        ];
+    } catch {
+        console.log('Error fetching lichess rating');
+    }
 }
 
 // probability of player with rating r1
 // winning against player with rating r2
 function winRate(r1, r2) {
-  const m = (r2 - r1) / 400;
-  const P = 1 / (1 + 10 ** m); //probability
-  const percent = Number.parseFloat(P * 100).toFixed(2); //percent 2 decimals
-  return percent;
+    const m = (r2 - r1) / 400;
+    const P = 1 / (1 + 10 ** m); //probability
+    const percent = Number.parseFloat(P * 100).toFixed(2); //percent 2 decimals
+    return percent;
 }
 
 async function fetchUser(username, site) {
-  if (username === "") {
-    return;
-  }
+    if (username === '') {
+        return;
+    }
 
-  if (site === "chesscom") {
-    return chesscom(username);
-  } else if (site === "lichess") {
-    return lichess(username);
-  } else {
-    console.log("Invalid website.");
-  }
+    if (site === 'chesscom') {
+        return chesscom(username);
+    } else if (site === 'lichess') {
+        return lichess(username);
+    } else {
+        console.log('Invalid website.');
+    }
 }
 
 export async function fetchStats() {
-  const site = document.querySelector("#site").value;
-  const time = document.querySelector("#time").value;
+    const site = document.querySelector('#site').value;
+    const time = document.querySelector('#time').value;
 
-  for (let i = 1; i <= 2; i++) {
-    const username = document.querySelector("#name" + i).value;
-    const out = document.querySelector("#r" + i);
-    try {
-      let obj = await fetchUser(username, site);
-      out.value = obj[time];
-    } catch {}
-  }
+    for (let i = 1; i <= 2; i++) {
+        const username = document.querySelector('#name' + i).value;
+        const out = document.querySelector('#r' + i);
+        try {
+            let obj = await fetchUser(username, site);
+            out.value = obj[time];
+        } catch {}
+    }
 }
 
 export function calculate() {
-  const r1 = document.querySelector("#r1").value;
-  const r2 = document.querySelector("#r2").value;
-  const out = document.querySelector("#winrate");
-  const wr = winRate(r1, r2);
-  out.innerHTML =
-    "Player 1 is expected to win <b>" + wr + "%</b> of games against Player 2.";
+    const r1 = document.querySelector('#r1').value;
+    const r2 = document.querySelector('#r2').value;
+    const out = document.querySelector('#winrate');
+    const wr = winRate(r1, r2);
+
+    if (r1 == '' || r2 == '' || isNaN(r1) || isNaN(r2)) {
+        return;
+    }
+
+    out.innerHTML =
+        'Player 1 is expected to win <b>' +
+        wr +
+        '%</b> of games against Player 2.';
 }
--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
-import { calculate, fetchStats } from "./chess.js";
+import { calculate, fetchStats } from './chess.js';
 
-const calcButton = document.querySelector("#calc");
-calcButton.addEventListener("click", (event) => {
-  calculate();
+const calcButton = document.querySelector('#calc');
+calcButton.addEventListener('click', (event) => {
+    calculate();
 });
 
-const fetchButton = document.querySelector("#fetch");
-fetchButton.addEventListener("click", (event2) => {
-  fetchStats();
+const fetchButton = document.querySelector('#fetch');
+fetchButton.addEventListener('click', (event2) => {
+    fetchStats();
 });
 
-document.addEventListener("keypress", function (event) {
-  if (event.key == "Enter") {
-    const focus = document.activeElement.className;
-    if (focus == "text username") {
-      fetchStats();
-    } else if (focus == "text rating") {
-      calculate();
+document.addEventListener('keypress', function (event) {
+    if (event.key == 'Enter') {
+        const focus = document.activeElement.className;
+        if (focus == 'text username') {
+            fetchStats();
+        } else if (focus == 'text rating') {
+            calculate();
+        }
     }
-  }
 });
